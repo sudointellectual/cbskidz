fa1154d25572d59f1e53889abba4a6185dd56be6
Jessica Rocha <jessica.r.lim@cbsinteractive.com>
2016-07-28 15:01:14 -0400

    so rusty lol

--- register.php
+++ register.php
@@ -19,19 +19,7 @@
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CBS Kids</a>
-        </div>
-      </div>
-    </nav>
+    <?php include('nav.php'); ?>
 
    <div class="container">
       <!-- Example row of columns -->
