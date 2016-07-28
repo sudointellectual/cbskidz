5cedd442e64769d6f6b44e46b824680a91b7c701
Jessica Rocha <jessica.r.lim@cbsinteractive.com>
2016-07-28 15:42:48 -0400

    parials

--- login.php
+++ login.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>CBS Kids</title>
-
-    <!-- Bootstrap -->
-    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="styles/css/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <?php include('nav.php'); ?>
-
-   <div class="container">
+<?php include('partials/header.php'); ?>
       <!-- Example row of columns -->
       <div class="row">
 
@@ -48,4 +25,4 @@
       <!-- end row -->
       </div>
 
-      <?php include('footer.php'); ?>
+      <?php include('partials/footer.php'); ?>
