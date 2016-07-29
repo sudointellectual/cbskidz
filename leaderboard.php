ef9357aab4bdcdcd4161b76fcc991940fdc8136b
Benjamin Lorteau <benjamin.lorteau@cbsinteractive.com>
2016-07-28 20:59:14 -0400

    Add leaderboard page

---
+++ leaderboard.php
@@ -0,0 +1,70 @@
+<?php include('partials/header.php'); ?>
+
+    <div class="row">
+        <h3>PS 187 4th grade</h3>
+
+        <div class="row">
+            <div class="col-md-6 col-xs-12 text-center">
+                <p><img src="images/gold-medal_256.png" alt=""></p>
+                <h2>1. <img src="images/avatars/bruce_lee_32.png" width="32" height="32" class="chavo" /> Jane Doese</h2>
+                <p>10.560 points</p>
+            </div>
+            <div class="col-md-3 col-xs-6 text-center pull-left">
+                <p><img src="images/silver-medal_128.png" alt=""></p>
+                <h3>2. <img src="images/avatars/boxeador_32.png" width="32" height="32" class="boxeador" /> John Does</h3>
+                <p>9.160 points</p>
+            </div>
+            <div class="col-md-3 col-xs-6 text-center">
+                <p><img src="images/bronze-medal_128.png" alt=""></p>
+                <h3>3. <img src="images/avatars/barbaman_32.png" width="32" height="32" class="barbaman" /> Johnny Does</h3>
+                <p>8.960 points</p>
+            </div>
+        </div>
+
+        <table class="table">
+            <tr>
+                <td>4.</td>
+                <td><img src="images/avatars/caradebolsa_32.png" width="32" height="32" class="chavo" /></td>
+                <td><a href="">John Doe</a></td>
+                <td>8.410 points</td>
+            </tr>
+            <tr>
+                <td>5.</td>
+                <td><img src="images/avatars/afro_32.png" width="32" height="32" class="chavo" /></td>
+                <td><a href="">Jane Doe</a></td>
+                <td>7.500 points</td>
+            </tr>
+            <tr>
+                <td>6.</td>
+                <td><img src="images/avatars/anciano_32.png" width="32" height="32" class="chavo" /></td>
+                <td><a href="">John Doe</a></td>
+                <td>7.200 points</td>
+            </tr>
+            <tr>
+                <td>7.</td>
+                <td><img src="images/avatars/alien_32.png" width="32" height="32" class="chavo" /></td>
+                <td><a href="">John Doe</a></td>
+                <td>5.610 points</td>
+            </tr>
+            <tr>
+                <td>8.</td>
+                <td><img src="images/avatars/astronauta_32.png" width="32" height="32" class="chavo" /></td>
+                <td><a href="">Jane Doe</a></td>
+                <td>4.250 points</td>
+            </tr>
+            <tr>
+                <td>9.</td>
+                <td><img src="images/avatars/chavo_32.png" width="32" height="32" class="chavo" /></td>
+                <td><a href="">Jane Doe</a></td>
+                <td>3.400 points</td>
+            </tr>
+            <tr>
+                <td>10.</td>
+                <td><img src="http://placekitten.com/g/32/32" alt="" class="img-rounded"></td>
+                <td><a href="">John Doe</a></td>
+                <td>1.200 points</td>
+            </tr>
+        </table>
+    </div>
+
+<?php include('partials/footer.php'); ?>
