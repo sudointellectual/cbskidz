b06f1e628bcf5740169fb9c263474cfda811c9a0
Benjamin Lorteau <benjamin.lorteau@cbsinteractive.com>
2016-07-29 12:17:51 -0400

    - Adding links to articles

--- classroom-student.php
+++ classroom-student.php
@@ -11,25 +11,25 @@
             <table class="table">
                 <tr>
                     <td><img src="http://placekitten.com/g/32/32" alt="" class="img-rounded"></td>
-                    <td><a href="">Article title 1</a></td>
+                    <td><a href="article.php?article=politics-1">Historic Nomination</a></td>
                     <td><span class="label label-default">+50 points</span></td>
                     <td><span class="label label-danger">Unclaimed</span></td>
                 </tr>
                 <tr>
                     <td><img src="http://placekitten.com/g/32/32" alt="" class="img-rounded"></td>
-                    <td><a href="">Article title 3</a></td>
+                    <td><a href="article.php?article=politics-2">Dallas Sheriff Lupe Valdez</a></td>
                     <td><span class="label label-default">+50 points</span></td>
                     <td><span class="label label-danger">Unclaimed</span></td>
                 </tr>
                 <tr>
                     <td><img src="http://placekitten.com/g/32/32" alt="" class="img-rounded"></td>
-                    <td><a href="">Article title 2</a></td>
+                    <td><a href="article.php?article=politics-1">Carole King</a></td>
                     <td><span class="label label-default">+50 points</span></td>
                     <td><span class="label label-success">Claimed</span></td>
                 </tr>
                 <tr>
                     <td><img src="http://placekitten.com/g/32/32" alt="" class="img-rounded"></td>
-                    <td><a href="">Article title 4</a></td>
+                    <td><a href="article.php?article=politics-1">Mysterious purple Sea Orb</a></td>
                     <td><span class="label label-default">+50 points</span></td>
                     <td><span class="label label-success">Claimed</span></td>
                 </tr>
